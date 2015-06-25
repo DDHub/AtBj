b31ede82c81a002a68448d6defc818321b1f14f9
DDHub <denzelw@163.com>
2015-06-25 21:15:11 +0800

    add LeakCanary

--- app/src/main/java/cc/ddhub/atbj/PictureApplication.java
+++ app/src/main/java/cc/ddhub/atbj/PictureApplication.java
@@ -1,10 +1,13 @@
 package cc.ddhub.atbj;
 
 import android.app.Application;
+import android.content.Context;
 import android.os.Environment;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import java.io.File;
 
@@ -12,6 +15,9 @@ import java.io.File;
  * Created by denzelw on 15/6/14.
  */
 public class PictureApplication extends Application {
+
+    private RefWatcher refWatcher;
+
     private static PictureApplication application;
 
     public static PictureApplication getInstance(){
@@ -27,6 +33,12 @@ public class PictureApplication extends Application {
 
         File file = new File(getPictureCachePath());
         file.mkdirs();
+
+        refWatcher = LeakCanary.install(this);
+    }
+
+    public static RefWatcher getRefWatcher(Context context){
+        return ((PictureApplication) context.getApplicationContext()).refWatcher;
     }
 
     public String getPictureCachePath(){
